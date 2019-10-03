42fd56c0ed4cd05055da9e926406f49f55f2e1cb
jayrwafu1115 <56108274+jayrwafu1115@users.noreply.github.com>
2019-10-03 15:58:36 +0800

    image viewer using pancardview

---
+++ ImageViewer/ViewModels/CardViewModel.cs
@@ -0,0 +1,89 @@
+﻿using PanCardView.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ImageViewer.ViewModels
+{
+    public sealed class CardsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _currentIndex;
+        private int _imageCount = 1078;
+
+        public CardsViewModel()
+        {
+            Items = new ObservableCollection<object>
+            {
+                new { Source = CreateSource(), Ind = _imageCount++, Color = Color.Red },
+                new { Source = CreateSource(), Ind = _imageCount++, Color = Color.Green },
+                new { Source = CreateSource(), Ind = _imageCount++, Color = Color.Gold },
+                new { Source = CreateSource(), Ind = _imageCount++, Color = Color.Silver },
+                new { Source = CreateSource(), Ind = _imageCount++, Color = Color.Blue }
+            };
+
+            PanPositionChangedCommand = new Command(v =>
+            {
+                if (IsAutoAnimationRunning || IsUserInteractionRunning)
+                {
+                    return;
+                }
+
+                var index = CurrentIndex + (bool.Parse(v.ToString()) ? 1 : -1);
+                if (index < 0 || index >= Items.Count)
+                {
+                    return;
+                }
+                CurrentIndex = index;
+            });
+
+            RemoveCurrentItemCommand = new Command(() =>
+            {
+                if (!Items.Any())
+                {
+                    return;
+                }
+                Items.RemoveAt(CurrentIndex.ToCyclingIndex(Items.Count));
+            });
+
+            GoToLastCommand = new Command(() =>
+            {
+                CurrentIndex = Items.Count - 1;
+            });
+        }
+
+        public ICommand PanPositionChangedCommand { get; }
+
+        public ICommand RemoveCurrentItemCommand { get; }
+
+        public ICommand GoToLastCommand { get; }
+
+        public int CurrentIndex
+        {
+            get => _currentIndex;
+            set
+            {
+                _currentIndex = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentIndex)));
+            }
+        }
+
+        public bool IsAutoAnimationRunning { get; set; }
+
+        public bool IsUserInteractionRunning { get; set; }
+
+        public ObservableCollection<object> Items { get; }
+
+        private string CreateSource()
+        {
+            var source = $"https://picsum.photos/500/500?image={_imageCount}";
+            return source;
+        }
+    }
+}
---
+++ ImageViewer/Views/CardView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ImageViewer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageViewer.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardView : ContentPage
+    {
+        public CardView()
+        {
+            InitializeComponent();
+
+            BindingContext = new CardsViewModel();
+
+        }
+    }
+}
